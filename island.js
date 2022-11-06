e1fd78e69d6cbea8453b0b96ddddf420811a27a8
sarahmoore <sarahnmoore19@gmail.com>
2022-11-06 04:31:33 -0500

    solved island problem

--- island.js
+++ island.js
@@ -1,20 +1,49 @@
-function getNeighbors(row, col, graph) {
+function getNeighbors(row, column, matrix) {
+  let newArr = [];
+  //top = row - 1;
+  if (row - 1 >= 0 && matrix[row - 1].length >= column + 1) {
+    if (matrix[row - 1][column] === 1)
+    newArr.push([row - 1, column]);
+  }
 
-  // Check top
+  //bottom = row + 1
+  if (matrix.length >= row + 2 && matrix[row + 1].length >= column + 1) {
+    if (matrix[row + 1][column] === 1)
+    newArr.push([row + 1, column]);
+  }
+  //left = column - 1;
+  if (column - 1 >= 0) {
+    if (matrix[row][column - 1] === 1)
+    newArr.push([row, column - 1]);
+  }
 
-  // Check bottom
+  //right = column + 1;
+  if (matrix[row].length >= column + 2) {
+    if (matrix[row][column + 1] === 1)
+    newArr.push([row, column + 1]);
+  }
 
-  // Check left
-
-  // Check right
-
-  // Return neighbors
-
-  // Your code here
+  return newArr;
 }
 
 
 function islandSize(row, col, graph) {
+  let queue = [[row, col]];
+  let set = new Set([[row, col].join(",")]);
+  while(queue.length) {
+    let curr = queue.pop();
+    let neighbors = getNeighbors(curr[0], curr[1], graph);
+    for (let ele of neighbors) {
+      if(!set.has(ele.join(","))) {
+        queue.unshift(ele);
+        set.add(ele.join(","));
+      }
+    }
+  }
+  return set.size;
+}
+
+function islandSize2(row, col, graph) {
 
   // Create a visited set to store visited nodes
 
